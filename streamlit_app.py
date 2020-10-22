e797dfb3046cbd71f13a2fa60f9a11f5ea17e0c2
ChrisDeverall <40525895+ChrisDeverall@users.noreply.github.com>
2020-10-22 23:14:19 +0100

    Final Streamlit App

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,24 @@ import altair as alt
 import json
 
 @st.cache
-def get_covid_data():
+def Your_Covid_Data():
   with open('repeated_data.json') as json_f:
     repeated_json = json.load(json_f)
     return alt.Data(values=repeated_json["features"])
 
 @st.cache
-def get_world_data():
+def Your_World_Data():
   with open('world_data.json') as json_f:
     world_json = json.load(json_f)
     return alt.Data(values=world_json["features"])
 
-expander = st.beta_expander("Instructions")
-expander.write("This visualization app allows you to view how the public sentiment of fear has changed in 15 countries as a result of the novel Coronavirus. By clicking a country on either of the two graphs below, you can view how the Fear Index varied in comparison to new Covid cases and deaths. \n\n By holding shift and pressing multiple countries you can view how Covid fear and Covid statistics vary among different countries. View the descriptions below to learn more about the Fear Index and Interesting Trends.")
+st.title("How Data-Driven are our Covid fears?")
 
-repeated_data = get_covid_data()
-world_data = get_world_data()
+expander = st.beta_expander("Instructions")
+expander.write("This visualization app allows you to view how the public sentiment of fear has changed in 15 countries as a result of the novel Coronavirus. By clicking a country on either of the two graphs below, you can view how the Fear Index varied in comparison to new Covid cases and deaths. \n\n By holding shift and pressing multiple countries you can view how Covid fear and Covid statistics vary among different countries. To learn more about Interesting Trends, the Fear Index and Variable Selection, please view the descriptions below. The graphs start at week 11 of the year 2020, which corresponds to the data March 9, 2020.")
+
+repeated_data = Your_Covid_Data()
+world_data = Your_World_Data()
 
 
 my_selector = alt.selection_multi(fields = ["properties.location"],init = [{"properties.location": "United States"}])
@@ -111,9 +113,11 @@ deaths = alt.Chart(repeated_data, title='Covid Deaths over Time').mark_line().en
 
 st.write((chart | world + one_country  + boundaries) & fear & cases  & deaths)
 
+expander = st.beta_expander("Interesting Trends")
+expander.write("Have a look at how the Fear Index precedes the number of infections in first world countries. In most of these countries, the fear index halves by the time Covid cases hit their peak. Do we just become bored when the pandemic is most deadly? \n\nNotice how the death rate is very low in UAE compared to in Ireland; perhaps the lower percentage of elderly could explain this. \n\n Have a look at Covid infections and deaths in third world countries. Could a lack of testing explain the low cases and deaths even when fear is high? \n\n Maybe look at how similar the fear and Covid trends are within countries of the same continent, maybe shared cultural and geographical aspects could be the reason?")
 
 expander = st.beta_expander("What is the Fear Index")
-expander.write("The Fear Index is the percentage of Covid-related tweets that contain keywords related to fear. These keywords include conjugations of multiple synonyms such as scared, afraid and nervous. \n\n To collect the Tweets, we utilize the publically available Harvard 'Coronavirus Tweet Ids' dataset.")
+expander.write("The Fear Index is the percentage of Covid-related tweets that contain keywords related to fear. These keywords include conjugations of synonyms of 'fear' such as 'scared', 'afraid' and 'nervous'. \n\n To collect the Tweets, we utilize the publically available Harvard 'Coronavirus Tweet Ids' dataset.")
 
-expander = st.beta_expander("Interesting Trends")
-expander.write("Try viewing how this and that and other information")
+expander = st.beta_expander("Variable Selection")
+expander.write("The variables GDP per Capita and percentage of population above 65 were chosen because our intuition would tell us that our Covid fears should increase as these variables increase. This is because Covid is more dangerous when hospital beds cannot be afforded or when it affects a member of the elderly.")
